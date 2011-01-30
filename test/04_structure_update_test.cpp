091ac3b054d4e6a2879e82af4672c87047a584fe
Joe Yates <joe.g.yates@gmail.com>
2011-01-30 18:54:35 +0000

    Added test for structure modification

---
+++ test/04_structure_update_test.cpp
@@ -0,0 +1,38 @@
+#include "test_helper.h"
+
+extern ActiveRecord::Connection connection;
+extern string database_file;
+
+class StructureUpdateTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    delete_database();
+    string create = "echo 'CREATE TABLE foo (bar INTEGER);' | sqlite3 " + database_file + ";";
+    system( create.c_str() );
+    connect_database();
+  }
+  virtual void TearDown() {
+    // delete_database();
+  }
+};
+
+TEST_F( StructureUpdateTest, LoadsSchema ) {
+  Table td;
+  td.connection = &connection;
+  td.table_name = "foo";
+  td.fields.push_back( Field( "foo", ActiveRecord::integer ) );
+  td.fields.push_back( Field( "baz", ActiveRecord::text ) );
+  td.fields.push_back( Field( "qux", ActiveRecord::floating_point ) );
+
+  connection.add_class( "Foo" );
+  ActiveRecord::tables[ "Foo" ] = td;
+
+  connection.update_database();
+
+  Schema schema   = connection.schema();
+  Table foo_table = schema[ "foo" ];
+  ASSERT_EQ( 4, foo_table.fields.size() );
+  assert_field( foo_table, 1, "foo", ActiveRecord::integer );
+  assert_field( foo_table, 2, "baz", ActiveRecord::text );
+  assert_field( foo_table, 3, "qux", ActiveRecord::floating_point );
+}
