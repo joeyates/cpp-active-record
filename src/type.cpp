44479e263fc753144c1c2c52534912abf7bdbfe7
Joe Yates <joe.g.yates@gmail.com>
2011-02-05 18:30:24 +0000

    Whitespace

--- src/type.cpp
+++ src/type.cpp
@@ -4,16 +4,16 @@ namespace ActiveRecord {
 
 TypeNamePair type_pairs[] =
 {
-TypeNamePair( ActiveRecord::integer, "INTEGER" ),
-TypeNamePair( ActiveRecord::text, "TEXT" ),
-TypeNamePair( ActiveRecord::floating_point, "REAL" )
+  TypeNamePair( ActiveRecord::integer,        "INTEGER" ),
+  TypeNamePair( ActiveRecord::text,           "TEXT" ),
+  TypeNamePair( ActiveRecord::floating_point, "REAL" )
 };
 
 Type type_list[] =
 {
-integer,
-text,
-floating_point
+  integer,
+  text,
+  floating_point
 };
 
 TypeNameMap type_name( type_pairs, type_pairs + sizeof( type_pairs ) / sizeof( type_pairs[ 0 ] ) );
