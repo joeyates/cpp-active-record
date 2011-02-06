1d80494b06ea4d88826075580643a5e411f291e0
Joe Yates <joe.g.yates@gmail.com>
2011-02-06 19:22:51 +0000

    United TableSet tests

--- test/table_set_test.cpp
+++ test/table_set_test.cpp
@@ -40,6 +40,29 @@ TEST_F( TableSetTest, UpdateDatabase ) {
   ASSERT_STREQ( "CREATE TABLE people (id INTEGER PRIMARY KEY, name TEXT, surname TEXT)", rows[ 0 ].get_text( "sql" ).c_str() );
 }
 
+class ReadSchemaTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    delete_database();
+    string create = "echo 'CREATE TABLE foo (bar INTEGER, baz TEXT, qux REAL);' | sqlite3 " + database_file + ";";
+    system( create.c_str() );
+    connect_database();
+  }
+  virtual void TearDown() {
+    delete_database();
+  }
+};
+
+TEST_F( ReadSchemaTest, LoadsSchema ) {
+  TableSet schema = TableSet::schema( &connection );
+  Table foo_table = schema[ "foo" ];
+
+  ASSERT_EQ( foo_table.fields().size(), 3 );
+  assert_field( foo_table, 0, "bar", ActiveRecord::integer );
+  assert_field( foo_table, 1, "baz", ActiveRecord::text );
+  assert_field( foo_table, 2, "qux", ActiveRecord::floating_point );
+}
+
 class StructureUpdateTest : public ::testing::Test {
  protected:
   virtual void SetUp() {
