7323f1ff729ea93a8b538d8bb182a245572c5fa7
Joe Yates <joe.g.yates@gmail.com>
2011-02-12 21:09:15 +0000

    Split Base tests into attribute tests and save tests

--- test/base_attribute_test.cpp
+++ test/base_attribute_test.cpp
@@ -5,6 +5,43 @@ extern string database_file;
 class BaseAttributeTest : public ::testing::Test {
  protected:
   virtual void SetUp() {
+    Person::setup( NULL );
+  }
+};
+
+TEST_F( BaseAttributeTest, Defaults ) {
+  Person joe;
+
+  ASSERT_EQ( -1, joe.integer( "id" ) );
+}
+
+TEST_F( BaseAttributeTest, SettingAttributesSingly ) {
+  Person joe;
+  joe[ "name" ]    = "Joe";
+  joe[ "surname" ] = "Yates";
+  joe[ "age" ]     = 45;
+  joe[ "height" ]  = 1.80;
+
+  assert_string( "Joe", joe.text( "name" ) );
+  ASSERT_EQ( 45, joe.integer( "age" ) );
+  ASSERT_DOUBLE_EQ( 1.80, joe.floating_point( "height" ) );
+}
+
+TEST_F( BaseAttributeTest, SettingAttributesUsingAttributesMethod ) {
+  Person joe( attributes
+              ( "name", "Joe" )
+              ( "surname", "Yates" )
+              ( "age", 45 )
+              ( "height", 1.80 ) );
+
+  assert_string( "Joe", joe.text( "name" ) );
+  ASSERT_EQ( 45, joe.integer( "age" ) );
+  ASSERT_DOUBLE_EQ( 1.80, joe.floating_point( "height" ) );
+}
+
+class BaseSaveTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
     delete_database();
     connect_database( connection, database_file );
     Person::setup( &connection );
@@ -17,29 +54,16 @@ class BaseAttributeTest : public ::testing::Test {
   Connection connection;
 };
 
-TEST_F( BaseAttributeTest, Defaults ) {
-  Person joe;
-
-  ASSERT_EQ( -1, joe.integer( "id" ) );
-}
-
-TEST_F( BaseAttributeTest, SetSingly ) {
+TEST_F( BaseSaveTest, Save ) {
   Person joe;
   joe[ "name" ]    = "Joe";
-  joe[ "surname" ] = "Yates";
-  joe[ "age" ]     = 45;
-  joe[ "height" ]  = 1.80;
 
   ASSERT_TRUE( joe.save() );
   ASSERT_TRUE( joe.integer( "id" ) > 0 );
 }
 
-TEST_F( BaseAttributeTest, SetUsingAttributes ) {
-  Person joe( attributes
-              ( "name", "Joe" )
-              ( "surname", "Yates" )
-              ( "age", 45 )
-              ( "height", 1.80 ) );
+TEST_F( BaseSaveTest, SaveWithNoData ) {
+  Person joe;
 
   ASSERT_TRUE( joe.save() );
   ASSERT_TRUE( joe.integer( "id" ) > 0 );
