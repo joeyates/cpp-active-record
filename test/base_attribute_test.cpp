8b28498a7210080a919b7ddfcd240fd38b8f0665
Joe Yates <joe.g.yates@gmail.com>
2011-02-12 20:38:51 +0000

    Added test for Record construction using the attributes() method.

--- test/base_attribute_test.cpp
+++ test/base_attribute_test.cpp
@@ -33,3 +33,14 @@ TEST_F( BaseAttributeTest, SetSingly ) {
   ASSERT_TRUE( joe.save() );
   ASSERT_TRUE( joe.integer( "id" ) > 0 );
 }
+
+TEST_F( BaseAttributeTest, SetUsingAttributes ) {
+  Person joe( attributes
+              ( "name", "Joe" )
+              ( "surname", "Yates" )
+              ( "age", 45 )
+              ( "height", 1.80 ) );
+
+  ASSERT_TRUE( joe.save() );
+  ASSERT_TRUE( joe.integer( "id" ) > 0 );
+}
