fa4f7b22b95f8cabc2f07e200eac9da86b7fc1ac
Joe Yates <joe.g.yates@gmail.com>
2011-02-14 23:06:04 +0000

    Separated database creation and database update tests

--- test/table_set_test.cpp
+++ test/table_set_test.cpp
@@ -66,6 +66,57 @@ TEST_F( ReadSchemaTest, LoadsSchema ) {
   assert_field( foo_table, 2, "qux", ActiveRecord::floating_point );
 }
 
+class TableSetCreateTableTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    delete_database();
+    connect_database( connection, database_file );
+  }
+  virtual void TearDown() {
+    delete_database();
+  }
+ protected:
+  Connection connection;
+};
+
+TEST_F( TableSetCreateTableTest, PrimaryKeyField ) {
+  Table td( &connection, "people" );
+  td.primary_key( "hi" );
+  td.fields().push_back( Field( "height",  ActiveRecord::floating_point ) );
+
+  TableSet tables;
+  tables[ "Person" ] = td;
+  tables.update_database();
+
+  // Tests: Check that we can call the primary key whatever we want
+  TableSet schema    = TableSet::schema( &connection );
+  Table people_table = schema[ "people" ];
+  ASSERT_EQ( people_table.fields().size(), 2 );
+  assert_field( people_table, 0, "hi",     ActiveRecord::integer );
+  assert_field( people_table, 1, "height", ActiveRecord::floating_point );
+}
+
+TEST_F( TableSetCreateTableTest, Timestamps ) {
+  Table td( &connection, "people" );
+  td.timestamps( true );
+  td.fields().push_back( Field( "name",    ActiveRecord::text ) );
+  td.fields().push_back( Field( "surname", ActiveRecord::text ) );
+
+  TableSet tables;
+  tables[ "Person" ] = td;
+  tables.update_database();
+
+  // Tests: Check for the timestamp fields
+  TableSet schema    = TableSet::schema( &connection );
+  Table people_table = schema[ "people" ];
+  ASSERT_EQ( people_table.fields().size(), 5 );
+  assert_field( people_table, 0, "id",         ActiveRecord::integer );
+  assert_field( people_table, 1, "name",       ActiveRecord::text );
+  assert_field( people_table, 2, "surname",    ActiveRecord::text );
+  assert_field( people_table, 3, "created_at", ActiveRecord::text );
+  assert_field( people_table, 4, "updated_at", ActiveRecord::text );
+}
+
 class TableSetUpdateDatabaseTest : public ::testing::Test {
  protected:
   virtual void SetUp() {
@@ -96,42 +147,4 @@ TEST_F( TableSetUpdateDatabaseTest, AddsFields ) {
   assert_field( foo_table, 0, "bar", ActiveRecord::integer );
   assert_field( foo_table, 1, "baz", ActiveRecord::text );
   assert_field( foo_table, 2, "qux", ActiveRecord::floating_point );
-}
-
-TEST_F( TableSetUpdateDatabaseTest, PrimaryKeyField ) {
-  Table td( &connection, "people" );
-  td.primary_key( "hi" );
-  td.fields().push_back( Field( "height",  ActiveRecord::floating_point ) );
-
-  TableSet tables;
-  tables[ "Person" ] = td;
-  tables.update_database();
-
-  // Tests: Check that we can call the primary key whatever we want
-  TableSet schema    = TableSet::schema( &connection );
-  Table people_table = schema[ "people" ];
-  ASSERT_EQ( people_table.fields().size(), 2 );
-  assert_field( people_table, 0, "hi",     ActiveRecord::integer );
-  assert_field( people_table, 1, "height", ActiveRecord::floating_point );
-}
-
-TEST_F( TableSetUpdateDatabaseTest, Timestamps ) {
-  Table td( &connection, "people" );
-  td.timestamps( true );
-  td.fields().push_back( Field( "name",    ActiveRecord::text ) );
-  td.fields().push_back( Field( "surname", ActiveRecord::text ) );
-
-  TableSet tables;
-  tables[ "Person" ] = td;
-  tables.update_database();
-
-  // Tests: Check for the timestamp fields
-  TableSet schema    = TableSet::schema( &connection );
-  Table people_table = schema[ "people" ];
-  ASSERT_EQ( people_table.fields().size(), 5 );
-  assert_field( people_table, 0, "id",         ActiveRecord::integer );
-  assert_field( people_table, 1, "name",       ActiveRecord::text );
-  assert_field( people_table, 2, "surname",    ActiveRecord::text );
-  assert_field( people_table, 3, "created_at", ActiveRecord::text );
-  assert_field( people_table, 4, "updated_at", ActiveRecord::text );
 }
