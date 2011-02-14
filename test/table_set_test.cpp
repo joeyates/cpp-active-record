503c11d42d008cb9d7cae072ed6b8ad7f8c6d757
Joe Yates <joe.g.yates@gmail.com>
2011-02-14 22:52:57 +0000

    Moved all TabeSet tests into one file

--- test/table_set_test.cpp
+++ test/table_set_test.cpp
@@ -67,7 +67,7 @@ TEST_F( ReadSchemaTest, LoadsSchema ) {
   assert_field( foo_table, 2, "qux", ActiveRecord::floating_point );
 }
 
-class StructureUpdateTest : public ::testing::Test {
+class TableSetUpdateDatabaseTest : public ::testing::Test {
  protected:
   virtual void SetUp() {
     delete_database();
@@ -81,7 +81,7 @@ class StructureUpdateTest : public ::testing::Test {
   Connection connection;
 };
 
-TEST_F( StructureUpdateTest, AddsFields ) {
+TEST_F( TableSetUpdateDatabaseTest, AddsFields ) {
   Table td( &connection, "foo" );
   td.fields().push_back( Field( "bar", ActiveRecord::integer ) );
   td.fields().push_back( Field( "baz", ActiveRecord::text ) );
@@ -97,4 +97,42 @@ TEST_F( StructureUpdateTest, AddsFields ) {
   assert_field( foo_table, 0, "bar", ActiveRecord::integer );
   assert_field( foo_table, 1, "baz", ActiveRecord::text );
   assert_field( foo_table, 2, "qux", ActiveRecord::floating_point );
+}
+
+TEST_F( TableSetUpdateDatabaseTest, PrimaryKeyField ) {
+  Table td( &connection, "people" );
+  td.primary_key( "hi" );
+  td.fields().push_back( Field( "height",  ActiveRecord::floating_point ) );
+
+  TableSet tables;
+  tables[ "Person" ] = td;
+  tables.update_database();
+
+  // Tests: Check that we can call the primary key whatever we want
+  TableSet schema    = TableSet::schema( &connection );
+  Table people_table = schema[ "people" ];
+  ASSERT_EQ( people_table.fields().size(), 2 );
+  assert_field( people_table, 0, "hi",     ActiveRecord::integer );
+  assert_field( people_table, 1, "height", ActiveRecord::floating_point );
+}
+
+TEST_F( TableSetUpdateDatabaseTest, Timestamps ) {
+  Table td( &connection, "people" );
+  td.timestamps( true );
+  td.fields().push_back( Field( "name",    ActiveRecord::text ) );
+  td.fields().push_back( Field( "surname", ActiveRecord::text ) );
+
+  TableSet tables;
+  tables[ "Person" ] = td;
+  tables.update_database();
+
+  // Tests: Check for the timestamp fields
+  TableSet schema    = TableSet::schema( &connection );
+  Table people_table = schema[ "people" ];
+  ASSERT_EQ( people_table.fields().size(), 5 );
+  assert_field( people_table, 0, "id",         ActiveRecord::integer );
+  assert_field( people_table, 1, "name",       ActiveRecord::text );
+  assert_field( people_table, 2, "surname",    ActiveRecord::text );
+  assert_field( people_table, 3, "created_at", ActiveRecord::text );
+  assert_field( people_table, 4, "updated_at", ActiveRecord::text );
 }
