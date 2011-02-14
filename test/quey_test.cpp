85125bf94908b90bfcaeb77fcc75bf8c86ecf247
Joe Yates <joe.g.yates@gmail.com>
2011-02-14 02:08:55 +0000

    Added tests for Query

---
+++ test/quey_test.cpp
@@ -0,0 +1,47 @@
+#include "test_helper.h"
+
+#include <active_record/query.h>
+
+extern string database_file;
+
+class QueryTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    delete_database();
+    connect_database( connection, database_file );
+    Person::setup( &connection );
+    ActiveRecord::tables.update_database();
+    pipe_to_sqlite( database_file, "INSERT INTO people (name, surname, age, height) VALUES (\"Joe\", \"Yates\", 45, 1.80);" );
+    pipe_to_sqlite( database_file, "INSERT INTO people (name, surname, age, height) VALUES (\"Joe\", \"Smith\", 45, 1.80);" );
+    pipe_to_sqlite( database_file, "INSERT INTO people (name, surname, age, height) VALUES (\"John\", \"Smith\", 67, 1.80);" );
+    pipe_to_sqlite( database_file, "INSERT INTO people (name, surname, age, height) VALUES (\"Frank\", \"Smith\", 45, 1.79);" );
+  }
+  virtual void TearDown() {
+    delete_database();
+  }
+ protected:
+  Connection connection;
+};
+
+TEST_F( QueryTest, All ) {
+  Query< Person > query;
+  vector< Person > people = query.all();
+
+  ASSERT_EQ( 4, people.size() );
+}
+
+TEST_F( QueryTest, Where ) {
+  Query< Person > query;
+  query.where( "surname = ?", "Smith" );
+  vector< Person > people = query.all();
+
+  ASSERT_EQ( 3, people.size() );
+}
+
+TEST_F( QueryTest, Limit ) {
+  Query< Person > query;
+  query.limit( 2 );
+  vector< Person > people = query.all();
+
+  ASSERT_EQ( 2, people.size() );
+}
