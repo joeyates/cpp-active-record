0a1ce00c909b3062823fd04879431733646b449a
Joe Yates <joe.g.yates@gmail.com>
2011-02-18 00:10:24 +0000

    Added a main() function so we can catch unhandled errors

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <active_record/exception.h>
+
+int main( int argc, char **argv ) {
+  ::testing::InitGoogleTest( &argc, argv );
+  int result = -1;
+  try {
+    result = RUN_ALL_TESTS();
+  }
+  catch( ActiveRecord::ActiveRecordException e) {
+    cout << "Test raised an unexpected exception:" << endl;
+    cout << e.to_string() << endl;
+    result = -1;
+  }
+  return result;
+}
