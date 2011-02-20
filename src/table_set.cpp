d63d0653e7884257a90e4de7dc87ab162b8fba60
Joe Yates <joe.g.yates@gmail.com>
2011-02-20 00:23:12 +0000

    Use standard Connection::select_all to get table info

--- src/table_set.cpp
+++ src/table_set.cpp
@@ -22,15 +22,15 @@ Table TableSet::table_data( Connection * connection,
                             const string &table_name ) {
   stringstream row_query;
   row_query << "PRAGMA table_info( \"" << table_name << "\" );";
-  string query         = row_query.str();
-  sqlite3_stmt *ppStmt = 0;
-  int prepare_result   = sqlite3_prepare_v2( connection->db_, query.c_str(), query.size(), &ppStmt, 0 );
   Table td( connection, table_name );
-  while( sqlite3_step( ppStmt ) == SQLITE_ROW ) {
+  RowSet rows = td.connection()->select_all( row_query.str() );
+  for( RowSet::iterator it = rows.begin();
+       it != rows.end();
+       ++it ) {
     // cid | name |    type | notnull | dflt_value | pk
     // 0   |  bar | INTEGER |       0 |            | 0
-    const char * name       = ( const char * ) sqlite3_column_text( ppStmt, 1 );
-    const char * type_name  = ( const char * ) sqlite3_column_text( ppStmt, 2 );
+    string name      = it->get_text( "name" );
+    string type_name = it->get_text( "type" );
     ActiveRecord::Type type = ActiveRecord::to_type( type_name );
     if( type == ActiveRecord::unknown ) {
       stringstream error;
