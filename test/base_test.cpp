314407eac46bbe0e7e3ff6df511c1669ca605cb6
Joe Yates <joe.g.yates@gmail.com>
2011-02-25 00:17:34 +0000

    Updated Base tests after introduction of state

--- test/base_test.cpp
+++ test/base_test.cpp
@@ -90,6 +90,7 @@ class BaseLoadTest : public ::testing::Test {
     connect_database( ActiveRecord::connection, database_file );
     Person::setup( &ActiveRecord::connection );
     ActiveRecord::connection.update_database();
+    pipe_to_sqlite( database_file, "INSERT INTO people (name, surname, age, height) VALUES (\"Joe\", \"Yates\", 45, 1.80);" );
   }
   virtual void TearDown() {
     delete_database();
@@ -99,7 +100,6 @@ class BaseLoadTest : public ::testing::Test {
 };
 
 TEST_F( BaseLoadTest, Default ) {
-  pipe_to_sqlite( database_file, "INSERT INTO people (name, surname, age, height) VALUES (\"Joe\", \"Yates\", 45, 1.80);" );
   Person joe( 1 );
 
   assert_string( "Joe", joe.text( "name" ) );
@@ -107,13 +107,40 @@ TEST_F( BaseLoadTest, Default ) {
   ASSERT_DOUBLE_EQ( 1.80, joe.floating_point( "height" ) );
 }
 
+TEST_F( BaseLoadTest, LoadInvalidId ) {
+  Person joe( 999 );
+
+  ASSERT_THROW( joe.text( "name" ) , ActiveRecordException );
+}
+
 TEST_F( BaseLoadTest, SquareBracketsOperator ) {
-  pipe_to_sqlite( database_file, "INSERT INTO people (name, surname, age, height) VALUES (\"Joe\", \"Yates\", 45, 1.80);" );
   Person joe( 1 );
 
   assert_attribute( "Joe", joe[ "name" ] );
   assert_attribute( 45,    joe[ "age" ] );
   assert_attribute( 1.80,  joe[ "height" ] );
+}
+
+TEST_F( BaseLoadTest, HasDataUninitialized ) {
+  Person joe;
+
+  ASSERT_FALSE( joe.has_data() );
+}
+
+TEST_F( BaseLoadTest, HasDataOnlyId ) {
+  Person joe( 1 );
+
+  ASSERT_FALSE( joe.has_data() );
+}
+
+TEST_F( BaseLoadTest, HasDataWithAttributes ) {
+  Person joe( attributes
+              ( "name", "Joe" )
+              ( "surname", "Yates" )
+              ( "age", 45 )
+              ( "height", 1.80 ) );
+
+  ASSERT_TRUE( joe.has_data() );
 }
 
 class BaseSaveTest : public ::testing::Test {
@@ -136,14 +163,14 @@ TEST_F( BaseSaveTest, Save ) {
   joe[ "name" ] = "Joe";
 
   ASSERT_TRUE( joe.save() );
-  ASSERT_TRUE( joe.integer( "id" ) > 0 );
+  ASSERT_TRUE( joe.id() > 0 );
 }
 
 TEST_F( BaseSaveTest, SaveWithNoData ) {
   Person joe;
 
   ASSERT_TRUE( joe.save() );
-  ASSERT_TRUE( joe.integer( "id" ) > 0 );
+  ASSERT_TRUE( joe.id() > 0 );
 }
 
 TEST_F( BaseSaveTest, SavedAttributes ) {
