70ee4050184db8617c08d88991637ca275792bd8
Joe Yates <joe.g.yates@gmail.com>
2011-03-08 23:35:39 +0000

    Optimization: avoid a Table construction and a copy

--- include/active_record/query.h
+++ include/active_record/query.h
@@ -105,7 +105,7 @@ QueryParametersPair Query< T >::condition_clause() {
 
 template < class T >
 QueryParametersPair Query< T >::query_and_parameters() {
-  Table t = ActiveRecord::connection.get_table( T::class_name );
+  const Table& t = ActiveRecord::connection.get_table( T::class_name );
   stringstream ss;
   ss << "SELECT ";
   ss << t.primary_key() << " ";
@@ -123,7 +123,7 @@ template < class T >
 vector< T > Query< T >::all() {
   QueryParametersPair query = query_and_parameters();
   RowSet rows               = connection.select_all( query.first, query.second );
-  Table t                   = connection.get_table( T::class_name );
+  const Table& t            = connection.get_table( T::class_name );
   string primary_key        = t.primary_key();
   vector< T > results;
   for( RowSet::iterator it = rows.begin(); it != rows.end(); ++it ) {
@@ -140,7 +140,7 @@ T Query< T >::first() {
   if( ! row.has_data() )
     throw ActiveRecordException( "No data", __FILE__, __LINE__ );
 
-  Table t                   = connection.get_table( T::class_name );
+  const Table& t            = connection.get_table( T::class_name );
   string primary_key        = t.primary_key();
   T record( row.get_integer( primary_key ) );
 
