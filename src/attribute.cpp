33b5601c9b656b11fb9402b1b89257853f535bce
Joe Yates <joe.g.yates@gmail.com>
2012-08-11 00:08:01 +0100

    Return an empty Attribute for NULL

--- src/attribute.cpp
+++ src/attribute.cpp
@@ -29,9 +29,13 @@ Attribute Attribute::from_field(sqlite3_stmt *pStmt, int i) {
   const char * type = sqlite3_column_decltype(pStmt, i);
   if(type == NULL) {
     // http://www.sqlite.org/capi3ref.html#sqlite3_column_decltype
+    // This is either an actual NULL value or SQLite doesn't know the
+    // declared type of the column.
     const char * value = (const char *) sqlite3_column_text(pStmt, i);
     if(value != 0)
       return value;
+    else
+      return Attribute();
   } else if(strcasecmp( type, "INTEGER") == 0) {
     return sqlite3_column_int( pStmt, i );
   } else if(strcasecmp( type, "FLOAT") == 0) {
@@ -40,10 +44,14 @@ Attribute Attribute::from_field(sqlite3_stmt *pStmt, int i) {
     const char * value = (const char *) sqlite3_column_text(pStmt, i);
     if(value != 0)
       return value;
+    else
+      return Attribute(); // TEXT hasn't been set
   } else if(strcasecmp(type, "DATE") == 0) {
     const char * value = (const char *) sqlite3_column_text(pStmt, i);
     if(value != 0)
       return Date::parse(value);
+    else
+      return Attribute(); // DATE hasn't been set
   } else {
     stringstream error;
     error << "Unhandled data type: " << type;
