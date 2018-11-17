85eba5d8501cc7a3971b81665fb748eea9b59458
Joe Yates <joe.g.yates@gmail.com>
2018-11-17 10:35:49 +0100

    Rename variable

--- test/postgresql_table_set_test.cpp
+++ test/postgresql_table_set_test.cpp
@@ -9,22 +9,22 @@ class PostgresqlTableSetTest : public PostgresqlTest {
     created_database_ = "active_record_test_database";
     postgresql_shell_create_database(created_database_, connection_options_);
     connection_options_["database"] = created_database_;
-    connection.connect(connection_options_);
+    connection_.connect(connection_options_);
    }
 
   virtual void TearDown() {
-    connection.disconnect();
+    connection_.disconnect();
     connection_options_["database"] = "template1";
     postgresql_shell_drop_database(created_database_, connection_options_);
   }
 
  protected:
-  PostgresqlConnection connection;
+  PostgresqlConnection connection_;
   string created_database_;
 };
 
 TEST_F(PostgresqlTableSetTest, CreateTable) {
-  Table td(&connection, "people");
+  Table td(&connection_, "people");
 
   TableSet::create_table(td);
 
